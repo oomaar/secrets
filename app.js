eb68b67bb5c763c6f7b646f148d662a40ccc6a39
oomaar <omaar_5@hotmail.com>
2020-10-20 19:29:30 +0200

    Hashing

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+// const encrypt = require("mongoose-encryption");
 
 // Router Declration and uses in this case it's called app
 const app = express();
@@ -23,7 +24,7 @@ const userSchema = new mongoose.Schema({
 });
 
 // Used .env file
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] }); //this will encrypt the entire database without encry. fields op.
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] }); //this will encrypt the entire database without encry. fields op.
 
 // Database model
 const User = new mongoose.model("user", userSchema);
@@ -39,7 +40,7 @@ app.get("/login", function(req, res) {
 
 app.post("/login", function(req, res) {
     const userName = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: userName }, function(err, foundUser) {
         if (err) {
@@ -61,7 +62,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err) {
