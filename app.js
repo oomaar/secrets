dd94ecafc968413ce799de78e78a11d582ad903d
oomaar <omaar_5@hotmail.com>
2020-10-23 18:59:05 +0200

    Passport Level 5

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 5;
-// const md5 = require("md5");
-// const encrypt = require("mongoose-encryption");
+//Auth npm 
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 // Router Declration and uses in this case it's called app
 const app = express();
@@ -16,8 +16,20 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Auth App.use
+app.use(session({
+    secret: "our little secret.",
+    resave: false,
+    saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Database Connection
 mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Error Auth
+mongoose.set('useCreateIndex', true);
 
 // Schema and it's Level 2 encryption
 const userSchema = new mongoose.Schema({
@@ -25,11 +37,19 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-// Used .env file
-// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] }); //this will encrypt the entire database without encry. fields op.
+// plugin auth
+userSchema.plugin(passportLocalMongoose);
 
 // Database model
 const User = new mongoose.model("user", userSchema);
+
+//auth passport uses
+// use static authenticate method of model in LocalStrategy
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Server Routes
 app.get("/", function(req, res) {
@@ -41,22 +61,28 @@ app.get("/login", function(req, res) {
 });
 
 app.post("/login", function(req, res) {
-    const userName = req.body.username;
-    const password = req.body.password;
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
-    User.findOne({ email: userName }, function(err, foundUser) {
+    req.logIn(user, function(err) {
         if (err) {
             console.log(err);
         } else {
-            if (foundUser) {
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                    if (result === true) {
-                        res.render("secrets");
-                    }
-                });
-            }
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            });
         }
     });
+});
+
+app.get("/secrets", function(req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
 });
 
 app.get("/register", function(req, res) {
@@ -64,32 +90,21 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-    // const newUser = new User({
-    //     email: req.body.username,
-    //     password: md5(req.body.password)
-    // });
-
-    // newUser.save(function(err) {
-    //     if (err) {
-    //         console.log(err);
-    //     } else {
-    //         res.render("secrets");
-    //     }
-    // });
-
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-        newUser.save(function(err) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("secrets");
-            }
-        });
+    User.register({ username: req.body.username }, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            });
+        }
     });
+});
+
+app.get("/logout", function(req, res) {
+    req.logOut();
+    res.redirect("/");
 });
 
 // Server is up and running on this port !
