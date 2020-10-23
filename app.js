26c52283de15d8c3e1d240c79afa807a0e865359
oomaar <omaar_5@hotmail.com>
2020-10-23 22:30:54 +0200

    Maybe that's the last update Level 6

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+//Level - 6
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 
 // Router Declration and uses in this case it's called app
 const app = express();
@@ -34,11 +38,14 @@ mongoose.set('useCreateIndex', true);
 // Schema and it's Level 2 encryption
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    facebookId: String
 });
 
 // plugin auth
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 // Database model
 const User = new mongoose.model("user", userSchema);
@@ -46,10 +53,47 @@ const User = new mongoose.model("user", userSchema);
 //auth passport uses
 // use static authenticate method of model in LocalStrategy
 passport.use(User.createStrategy());
-
 // use static serialize and deserialize of model for passport session support
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+//These uses is replaced with the passport serializing logic from there docs !
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+// Stratgies Level - 6
+// google strategy
+passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/secrets",
+        // Retriving google+ info with this url ref: video(11) 23:08 mins and someon's github pullrequested this
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+        passReqToCallback: true
+    },
+    function(request, accessToken, refreshToken, profile, done) {
+        User.findOrCreate({ googleId: profile.id }, function(err, user) {
+            return done(err, user);
+        }); //if this function find or create does'nt work, npm i mongoose-findorcreate and folow it's npm docs
+    }
+));
+
+//facebook strategy
+passport.use(new FacebookStrategy({
+        clientID: process.env.FACEBOOK_APP_ID,
+        clientSecret: process.env.FACEBOOK_APP_SECRET,
+        callbackURL: "http://localhost:3000/auth/facebook/secrets"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ facebookId: profile.id }, function(err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
 // Server Routes
 app.get("/", function(req, res) {
@@ -106,6 +150,28 @@ app.get("/logout", function(req, res) {
     req.logOut();
     res.redirect("/");
 });
+
+//Level - 6 Routes
+app.get("/auth/google",
+    passport.authenticate('google', {
+        scope: ["email", "profile"]
+    }));
+
+app.get("/auth/google/secrets",
+    passport.authenticate("google", {
+        successRedirect: "/secrets",
+        failureRedirect: "/login"
+    }));
+
+app.get("/auth/facebook",
+    passport.authenticate("facebook"));
+
+app.get('/auth/facebook/secrets',
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/secrets');
+    });
 
 // Server is up and running on this port !
 app.listen(3000, function() {
